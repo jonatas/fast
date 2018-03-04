67b5a27f50b4ad7f6b70fcc44d9040457ed1ecd1
Jônatas Davi Paganini <jonatas.paganini@toptal.com>
2018-03-04 10:03:57 -0300

    Add find usage example

---
+++ examples/find_usage.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# List files that matches with some expression
+# Usage:
+#
+# ruby examples/find_usage.rb defs
+#
+# Or be explicit about directory or folder:
+#
+# ruby examples/find_usage.rb defs lib/
+$LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
+
+require 'fast'
+require 'coderay'
+
+arguments = ARGV
+pattern = arguments.shift
+files = Fast.ruby_files_from(arguments.any? ? arguments : '.')
+files.select do |file|
+  begin
+    puts file if Fast.search_file(pattern, file).any?
+  rescue Parser::SyntaxError
+    []
+  end
+end
