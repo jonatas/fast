efdfe710e5b3c79756fa96de9d243bc9bd26f602
Jônatas Davi Paganini <jonatas.paganini@toptal.com>
2018-08-21 16:43:36 -0300

    Add Let it be experiment

---
+++ examples/let_it_be_experiment.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require '../../fast/lib/fast'
+
+# For specs using `let(:something) { create ... }` it tries to use `let_it_be` instead
+Fast.experiment('RSpec/LetItBe') do
+  lookup 'spec/models'
+  search '(block $(send nil let (sym _)) (args) (send nil create))'
+  edit { |_, (let)| replace(let.loc.selector, 'let_it_be') }
+  policy { |new_file| system("bin/spring rspec --fail-fast #{new_file}") }
+end.run
