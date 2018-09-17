3faf1a05addbe7c1a0340e06178601ec9e6b0529
Jônatas Davi Paganini <jonatas.paganini@toptal.com>
2018-09-17 20:22:02 -0300

    Use progress format

--- experiments/replace_create_with_build_stubbed.rb
+++ experiments/replace_create_with_build_stubbed.rb
@@ -6,5 +6,5 @@ Fast.experiment('RSpec/ReplaceCreateWithBuildStubbed') do
   lookup 'spec'
   search '(block (send nil let (sym _)) (args) $(send nil create))'
   edit { |_, (create)| replace(create.loc.selector, 'build_stubbed') }
-  policy { |new_file| system("bin/spring rspec --fail-fast #{new_file}") }
+  policy { |new_file| system("bin/spring rspec --format progress --fail-fast #{new_file}") }
 end
