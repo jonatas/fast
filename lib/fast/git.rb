6c2a28625f80df1dc0aef10f0f6429eda7da9c41
Jônatas Davi Paganini <jonatasdp@gmail.com>
2020-09-17 11:21:15 -0300

    Add node methods to get permalink

--- lib/fast/git.rb
+++ lib/fast/git.rb
@@ -32,5 +32,69 @@ module Fast
     def last_commit
       git_log.first
     end
+
+    # @return [String] with last commit SHA
+    def sha
+      last_commit.sha
+    end
+
+    # @return [String] with remote URL
+    def remote_url
+      git.remote.url
+    end
+
+    # Given #remote_url is "git@github.com:namespace/project.git"
+    # @return [String] "https://github.com/namespace/project"
+    def project_url
+      return remote_url if remote_url.start_with?("https")
+      remote_url
+        .gsub("git@","https://")
+        .gsub(/:(\w)/,"/\\1")
+        .gsub(/\.git$/,'')
+    end
+
+    def file
+      buffer_name.gsub(Dir.pwd + '/', '')
+    end
+
+
+
+    # @return
+    def gh_line_range
+      lines.map { |l| "L#{l}" }.join('-')
+    end
+    # @return [Array] with lines range
+    def lines
+      exp = loc.expression
+      first_line = exp.first_line
+      last_line = exp.last_line
+      [first_line, last_line].uniq
+    end
+
+    # @return [Integer] lines of code from current block
+    def lines_of_code
+      lines.last - lines.first + 1
+    end
+
+    # @return [String] a markdown link with #md_link_description and #github_link
+    def md_link(text = md_link_description)
+      "[#{text}](#{github_link})"
+    end
+
+    # @return [String] with the source cutting arguments from method calls to be
+    # able to create a markdown link without parens.
+    def md_link_description
+      source[/([^\r\(]+)\(/, 1] || source
+    end
+
+    # @return [String] with formmatted Github link
+    def github_link
+      "#{project_url}/blob/master/#{buffer_name}##{gh_line_range}"
+    end
+
+    # @return [String] with permanent link to the actual commit
+    def permalink
+      "#{project_url}/blob/#{sha}/#{buffer_name}##{gh_line_range}"
+    end
   end
 end
