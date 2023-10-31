be439a2cd1bbfadad5dcea9d1f6d3dc7d8eadb09
Jônatas Davi Paganini <jonatasdp@gmail.com>
2023-10-31 10:17:50 -0300

    Add basic spec for sql basics

--- lib/fast/sql.rb
+++ lib/fast/sql.rb
@@ -5,45 +5,49 @@ module Fast
 
   def parse_sql(statement)
     return [] if statement.nil?
-    stmts = sql_to_h(statement).values.map do |v|
-      to_sql_ast(v)
-    end
+    stmts = sql_to_h(statement).map do |v|
+      sql_tree_to_ast(v)
+    end.flatten
     stmts.one? ? stmts.first : stmts
   end
 
   # Transform a sql statement into a hash
   def sql_to_h(statement)
     tree = PgQuery.parse(statement).tree
-    clean_structure(tree.to_h)
+    tree.to_h[:stmts].map{|e|clean_structure(e[:stmt])}
   end
 
   # Clean up the hash structure returned by PgQuery
   def clean_structure(hash)
-    hash.delete(:version)
     res_hash = hash.map do |key, value|
       value = clean_structure(value) if value.is_a?(Hash)
       value = value.map(&Fast.method(:clean_structure)) if value.is_a?(Array)
+      value = nil if key.to_s =~ /_(location|len)$/ || key == :location
       value = nil if [{}, [], "", :SETOP_NONE, :LIMIT_OPTION_DEFAULT, false].include?(value)
       [key, value]
     end
     res_hash.to_h.compact
   end
 
-  def to_sql_ast(obj)
+  def sql_tree_to_ast(obj)
     case obj
     when Array
-      obj.map(&Fast.method(:to_sql_ast))
+      obj.map(&Fast.method(:sql_tree_to_ast)).flatten.compact
     when Hash
-      hash.map do |key, value|
-        children = 
-          case value
-          when Hash
-            hash[key] = to_sql_ast(value)
-          end
-        Node.new(key, *[children])
-      end
+      source_map = {}
+      obj.map do |key, value|
+        case key
+        when /_(location|len)$/, :location
+          source_map[key] = value
+          next
+        end
+        n = Node.new(key, [*sql_tree_to_ast(value)], source_map)
+        source_map = {}
+        n
+      end.compact
     else
       obj
     end
   end
 end
+
--- spec/fast/sql_spec.rb
+++ spec/fast/sql_spec.rb
@@ -3,35 +3,89 @@ require 'spec_helper'
 RSpec.describe Fast do
   describe ".parse_sql" do
     let(:ast) { described_class.parse_sql(sql) }
-    context "when sql is nil" do
-      let(:sql) { nil }
-      it { expect(ast).to eq([]) }
-    end
 
     context "when query sql is a plain statement" do
       describe "integer" do
         let(:sql) { "select 1" }
         it do
-          expect(ast).to eq(s(:select, s(:integer, 1)))
+          expect(ast).to eq(
+              s(:select_stmt,
+                s(:target_list,
+                  s(:res_target,
+                    s(:val,
+                      s(:a_const,
+                        s(:val,
+                          s(:integer,
+                            s(:ival, 1)))))))))
+        end
+      end
+    end
+
+    context "when query sql is a plain statement" do
+      describe "integer" do
+        let(:sql) { "select 1" }
+        it "simplifies the AST" do
+          expect(ast).to eq( s(:select_stmt,
+                               s(:target_list,
+                                 s(:res_target,
+                                   s(:val,
+                                     s(:a_const,
+                                       s(:val,
+                                         s(:integer,
+                                           s(:ival, 1)))))))))
         end
       end
       describe "string" do
         let(:sql) { "select 'hello'" }
         it do
-          expect(ast).to eq(s(:select, s(:string, "hello")))
+          expect(ast).to eq(
+            s(:select_stmt,
+              s(:target_list,
+                s(:res_target,
+                  s(:val,
+                    s(:a_const,
+                      s(:val,
+                        s(:string,
+                          s(:str, "hello")))))))))
         end
       end
       describe "float" do
         let(:sql) { "select 1.0" }
         it do
-          expect(ast).to eq(s(:select, s(:float, 1.0)))
+          expect(ast).to eq(
+            s(:select_stmt,
+              s(:target_list,
+                s(:res_target,
+                  s(:val,
+                    s(:a_const,
+                      s(:val,
+                        s(:float,
+                          s(:str, "1.0")))))))))
         end
       end
 
       describe "array" do
         let(:sql) { "select array[1,2,3]" }
         it do
-          expect(ast).to eq(s(:select, s(:array, [s(:integer, 1), s(:integer, 2), s(:integer, 3)])))
+          expect(ast).to eq(
+            s(:select_stmt,
+              s(:target_list,
+                s(:res_target,
+                  s(:val,
+                    s(:a_array_expr,
+                      s(:elements,
+                        s(:a_const,
+                          s(:val,
+                            s(:integer,
+                              s(:ival, 1)))),
+                        s(:a_const,
+                          s(:val,
+                            s(:integer,
+                              s(:ival, 2)))),
+                        s(:a_const,
+                          s(:val,
+                            s(:integer,
+                              s(:ival, 3)))))))))))
         end
       end
     end
@@ -40,9 +94,81 @@ RSpec.describe Fast do
       let(:sql) { "select 1; select 2;" }
       specify do
         expect(ast).to eq([
-          s(:select, s(:integer, 1)),
-          s(:select, s(:integer, 2))
-        ])
+          s(:select_stmt,
+            s(:target_list,
+              s(:res_target,
+                s(:val,
+                  s(:a_const,
+                    s(:val,
+                      s(:integer,
+                        s(:ival, 1)))))))),
+          s(:select_stmt,
+            s(:target_list,
+              s(:res_target,
+                s(:val,
+                  s(:a_const,
+                    s(:val,
+                      s(:integer,
+                        s(:ival, 2))))))))])
+      end
+    end
+    context "when use alias" do
+      let(:sql) { "select 1 as a" }
+      specify do
+        expect(ast).to eq(
+           s(:select_stmt,
+         s(:target_list,
+           s(:res_target,
+             s(:name, "a"),
+             s(:val,
+               s(:a_const,
+                 s(:val,
+                   s(:integer,
+                     s(:ival, 1)))))))))
+      end
+    end
+    context "when use from" do
+      let(:sql) { "select 1 from a" }
+      specify do
+        expect(ast).to eq(
+          s(:select_stmt,
+            s(:target_list,
+              s(:res_target,
+                s(:val,
+                  s(:a_const,
+                    s(:val,
+                      s(:integer,
+                        s(:ival, 1))))))),
+            s(:from_clause,
+              s(:range_var,
+                s(:relname, "a"),
+                s(:inh, true),
+                s(:relpersistence, "p")))))
+      end
+    end
+
+    context "when use group by" do
+      let(:sql) { "select 1 from a group by 1" }
+      specify do
+        expect(ast).to eq(
+          s(:select_stmt,
+            s(:target_list,
+              s(:res_target,
+                s(:val,
+                  s(:a_const,
+                    s(:val,
+                      s(:integer,
+                        s(:ival, 1))))))),
+          s(:from_clause,
+            s(:range_var,
+              s(:relname, "a"),
+              s(:inh, true),
+              s(:relpersistence, "p"))),
+          s(:group_clause,
+            s(:a_const,
+              s(:val,
+                s(:integer,
+                  s(:ival, 1)))))))
       end
     end
   end
